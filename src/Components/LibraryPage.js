448089a4c9974d7df058850ea8712ca6ef1c615b
Ritik Choudhary <ritikchoudharydd@gmail.com>
2021-09-12 03:54:43 +0530

    library space

--- src/Components/LibraryPage.js
+++ src/Components/LibraryPage.js
@@ -86,6 +86,7 @@ export default function LibraryPage() {
                       }
                     })
                   }
+                  return
                 })
               }}
             </WorkspaceConsumer>
@@ -154,6 +155,7 @@ export default function LibraryPage() {
                       }
                     })
                   }
+                  return
                 })
               }}
             </WorkspaceConsumer>
